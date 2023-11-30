4b2cf93dc9693ff1451ae0def5c99fee72e6e5c4
zasuklotur <143523477+zasuklotur@users.noreply.github.com>
2023-11-30 08:38:28 +0100

    Create code_1.py

---
+++ code_1.py
@@ -0,0 +1,39 @@
+if __name__ == "__main__":
+
+
+def main():
+    data = generate_random_data()
+    main()
+    data = [random.randint(1, 100) for _ in range(10)]
+    for item in data:
+import random
+
+    return data
+
+def generate_random_data():
+        print(f"Random Number: {item}")    for item in data:
+
+import random
+if __name__ == "__main__":
+def main():
+def generate_random_data():
+    main()
+
+
+
+    data = [random.randint(1, 100) for _ in range(10)]
+    data = generate_random_data()
+    return data
+        print(f"Random Number: {item}")if __name__ == "__main__":
+
+
+    data = generate_random_data()
+    data = [random.randint(1, 100) for _ in range(10)]
+def main():
+    main()
+import random
+def generate_random_data():
+    return data
+
+    for item in data:
+        print(f"Random Number: {item}")
